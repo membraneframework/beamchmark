19f85ce97a2314270b6fc79a5af17c3f600d3080
Jakub Balinski <balinski.jakub@gmail.com>
2022-03-02 12:58:13 +0100

    adjust to CR suggestions

--- examples/advanced.exs
+++ examples/advanced.exs
@@ -3,9 +3,19 @@ defmodule AdvancedScenario do
 
   @impl true
   def run() do
-    Enum.map(1..1_000_000, fn i -> Integer.pow(i, 2) end)
+    Enum.map(1..100_000_000, fn i -> Integer.pow(i, 2) end)
     :ok
   end
 end
 
-Beamchmark.run(AdvancedScenario, duration: 10, delay: 1)
+Beamchmark.run(AdvancedScenario,
+  duration: 10,
+  delay: 1,
+  compare?: true,
+  output_dir: "beamchmark",
+  formatters: [
+    Beamchmark.Formatters.Console,
+    {Beamchmark.Formatters.HTML,
+     [output_path: "reports/beamchmark.html", auto_open?: true, inline_assets?: false]}
+  ]
+)
