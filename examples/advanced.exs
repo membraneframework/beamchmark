77a95efa7d2c88e5921f9d6371d8cb2b62a07700
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2022-03-08 12:26:13 +0100

    Fixed examples/advanced.exs

--- examples/advanced.exs
+++ examples/advanced.exs
@@ -14,17 +14,4 @@ defmodule AdvancedScenario do
   end
 end
 
-# :cpu_sup.start()
-# task1 = Task.async(fn -> Beamchmark.run(AdvancedScenario, [duration: 10, delay: 1, formatters: [Beamchmark.Formatters.HTML]]) end)
-# IO.puts("Sleeping for some time")
-# Process.sleep(2000)
-# IO.puts("Awaiking")
-# task = Task.async(fn ->
-#   AdvancedScenario.run_and_print()
-#   end)
-# Task.await(task1,100000)
-# Task.shutdown(task)
-# Process.sleep(1000)
-# IO.inspect :cpu_sup.util([:per_cpu])
-
 Beamchmark.run(AdvancedScenario, duration: 10, delay: 1, formatters: [Beamchmark.Formatters.HTML])
