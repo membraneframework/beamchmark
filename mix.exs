c210a68c604d5dad4fead75c0d18fde62d48fac3
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:02 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -39,16 +39,15 @@ defmodule Beamchmark.MixProject do
     [
       {:bunch, "~> 1.5"},
       {:math, "~> 0.7.0"},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:mix, :syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
