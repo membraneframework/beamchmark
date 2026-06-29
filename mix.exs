b6845fc6ec38c72b83558f594c349e7a0ceaa021
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:25:26 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Beamchmark.MixProject do
       {:math, "~> 0.7.0"},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false}
     ]
   end
 
