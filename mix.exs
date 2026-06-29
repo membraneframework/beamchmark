c1e206fd5fdc2c58668075b96ac7dad03353a7b4
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:11:15 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Beamchmark.MixProject do
       source_url: @github_url,
       homepage_url: "https://membraneframework.org",
       docs: docs(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -81,7 +81,7 @@ defmodule Beamchmark.MixProject do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
